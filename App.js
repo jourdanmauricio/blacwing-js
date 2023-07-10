08a0d3a76656d00a7bfd6e10d97c5f3d445d2f82
Lucas Burriel <burriellucas@gmail.com>
2023-07-10 03:58:10 -0300

    trabajo 2

--- App.js
+++ App.js
@@ -1,34 +1,89 @@
-// Función para calcular el préstamo
-function calcularPrestamo(monto, tasaInteres, plazo) {
-    const cuota = monto * (tasaInteres / 100) / (1 - Math.pow(1 + tasaInteres / 100, -plazo));
-    return cuota.toFixed(2);
+// Declaración de variables y objetos necesarios
+
+const productos = []; // Array para almacenar los productos
+
+// Objeto Producto
+function Producto(nombre, categoria, precio) {
+  this.nombre = nombre;
+  this.categoria = categoria;
+  this.precio = precio;
+}
+
+// Función para agregar un producto
+function agregarProducto() {
+  const nombre = prompt("Ingrese el nombre del producto:");
+  const categoria = prompt("Ingrese la categoría del producto:");
+  const precio = parseFloat(prompt("Ingrese el precio del producto:"));
+
+  const producto = new Producto(nombre, categoria, precio);
+  productos.push(producto);
+
+  console.log("¡Producto agregado con éxito!");
+}
+
+// Función para buscar un producto por su nombre
+function buscarProducto() {
+  const nombre = prompt("Ingrese el nombre del producto a buscar:");
+  
+  const productoEncontrado = productos.find(producto => producto.nombre === nombre);
+
+  if (productoEncontrado) {
+    console.log("Producto encontrado:");
+    console.log(productoEncontrado);
+  } else {
+    console.log("No se encontró ningún producto con ese nombre.");
   }
-  
-  // Obtener los datos del usuario
-  let montoPrestamo = parseFloat(prompt("Ingrese el monto del préstamo:"));
-  let tasaInteresAnual = parseFloat(prompt("Ingrese la tasa de interés anual (%):"));
-  let plazoPrestamo = parseInt(prompt("Ingrese el plazo del préstamo en meses:"));
-  
-  // Calcular la cuota mensual
-  let cuotaMensual = calcularPrestamo(montoPrestamo, tasaInteresAnual, plazoPrestamo);
-  
-  // Imprimir el resultado
-  console.log("La cuota mensual es: $" + cuotaMensual);
-  
-  // Solicitar confirmación para calcular una nueva cuota
-  let calcularNuevaCuota = prompt("¿Desea calcular una nueva cuota? (S/N)").toUpperCase();
-  
-  // Ciclo para calcular nuevas cuotas
-  while (calcularNuevaCuota === "S") {
-    montoPrestamo = parseFloat(prompt("Ingrese el monto del préstamo:"));
-    tasaInteresAnual = parseFloat(prompt("Ingrese la tasa de interés anual (%):"));
-    plazoPrestamo = parseInt(prompt("Ingrese el plazo del préstamo en meses:"));
-    
-    cuotaMensual = calcularPrestamo(montoPrestamo, tasaInteresAnual, plazoPrestamo);
-    console.log("La cuota mensual es: $" + cuotaMensual);
-    
-    calcularNuevaCuota = prompt("¿Desea calcular una nueva cuota? (S/N)").toUpperCase();
+}
+
+// Función para filtrar productos por categoría
+function filtrarPorCategoria() {
+  const categoria = prompt("Ingrese la categoría para filtrar los productos:");
+
+  const productosFiltrados = productos.filter(producto => producto.categoria === categoria);
+
+  if (productosFiltrados.length > 0) {
+    console.log("Productos encontrados:");
+    console.table(productosFiltrados);
+  } else {
+    console.log("No se encontraron productos en esa categoría.");
   }
-  
-  console.log("¡Gracias por usar la calculadora de préstamos!");
-  
+}
+
+
+// Agregar algunos productos de ejemplo
+const producto1 = new Producto("Camiseta", "Ropa", 29.99);
+const producto2 = new Producto("Zapatos", "Calzado", 59.99);
+const producto3 = new Producto("Gorra", "Accesorios", 9.99);
+
+productos.push(producto1, producto2, producto3);
+
+// Ejecución del programa
+
+console.log("Bienvenido al sistema de gestión de productos.");
+
+while (true) {
+  const opcion = parseInt(prompt(
+    "Seleccione una opción:\n" +
+    "1. Agregar un producto\n" +
+    "2. Buscar un producto por nombre\n" +
+    "3. Filtrar productos por categoría\n" +
+    "4. Salir"
+  ));
+
+  switch (opcion) {
+    case 1:
+      agregarProducto();
+      break;
+    case 2:
+      buscarProducto();
+      break;
+    case 3:
+      filtrarPorCategoria();
+      break;
+    case 4:
+      console.log("¡Hasta luego!");
+      break; // Reemplazamos "return" por "break"
+    default:
+      console.log("Opción inválida. Intente nuevamente.");
+  }
+}
