ad8a2d54eaea43f2c085b4010526aabba21d974f
Lucas Burriel <burriellucas@gmail.com>
2023-07-15 17:20:22 -0300

    Tercer entrega v1

--- App.js
+++ App.js
@@ -1,82 +1,59 @@
-// Declaración de variables y objetos necesarios
+// app.js
 
-const productos = []; // Array para almacenar los productos
+// Obtener elementos del DOM
+const input = document.getElementById('taskInput');
+const addButton = document.getElementById('addButton');
+const list = document.getElementById('taskList');
 
-// Objeto Producto
-function Producto(nombre, categoria, precio) {
-  this.nombre = nombre;
-  this.categoria = categoria;
-  this.precio = precio;
-}
+// Función para agregar una nueva tarea a la lista
+function agregarTarea() {
+  const tareaTexto = input.value.trim();
 
-// Función para agregar un producto
-function agregarProducto() {
-  const nombre = prompt("Ingrese el nombre del producto:");
-  const categoria = prompt("Ingrese la categoría del producto:");
-  const precio = parseFloat(prompt("Ingrese el precio del producto:"));
+  if (tareaTexto !== '') {
+    const elementoLista = document.createElement('li');
+    elementoLista.textContent = tareaTexto;
 
-  const producto = new Producto(nombre, categoria, precio);
-  productos.push(producto);
+    const botonEliminar = document.createElement('button');
+    botonEliminar.textContent = 'Eliminar';
+    botonEliminar.addEventListener('click', () => {
+      list.removeChild(elementoLista);
+      guardarTareasEnStorage(); // Actualizamos el almacenamiento local al eliminar una tarea
+    });
 
-  console.log("¡Producto agregado con éxito!");
-}
-
-// Función para buscar un producto por su nombre
-function buscarProducto() {
-  const nombre = prompt("Ingrese el nombre del producto a buscar:");
-  
-  const productoEncontrado = productos.find(producto => producto.nombre === nombre);
-
-  if (productoEncontrado) {
-    console.log("Producto encontrado:");
-    console.log(productoEncontrado);
-  } else {
-    console.log("No se encontró ningún producto con ese nombre.");
+    elementoLista.appendChild(botonEliminar);
+    list.appendChild(elementoLista);
+    input.value = '';
+    guardarTareasEnStorage(); // Actualizamos el almacenamiento local al agregar una tarea
   }
 }
 
-// Función para filtrar productos por categoría
-function filtrarPorCategoria() {
-  const categoria = prompt("Ingrese la categoría para filtrar los productos:");
+// Agregar evento al botón para agregar tarea
+addButton.addEventListener('click', agregarTarea);
 
-  const productosFiltrados = productos.filter(producto => producto.categoria === categoria);
+// Función para cargar las tareas desde el almacenamiento local
+function cargarTareasDesdeStorage() {
+  const tareas = JSON.parse(localStorage.getItem('tareas')) || [];
+  tareas.forEach(tarea => {
+    const elementoLista = document.createElement('li');
+    elementoLista.textContent = tarea;
 
-  if (productosFiltrados.length > 0) {
-    console.log("Productos encontrados:");
-    console.log(productosFiltrados);
-  } else {
-    console.log("No se encontraron productos en esa categoría.");
-  }
+    const botonEliminar = document.createElement('button');
+    botonEliminar.textContent = 'Eliminar';
+    botonEliminar.addEventListener('click', () => {
+      list.removeChild(elementoLista);
+      guardarTareasEnStorage(); // Actualizamos el almacenamiento local al eliminar una tarea
+    });
+
+    elementoLista.appendChild(botonEliminar);
+    list.appendChild(elementoLista);
+  });
 }
 
-// Ejecución del programa
+// Función para guardar las tareas en el almacenamiento local
+function guardarTareasEnStorage() {
+  const tareas = Array.from(list.children).map(li => li.textContent);
+  localStorage.setItem('tareas', JSON.stringify(tareas));
+}
 
-console.log("Bienvenido al sistema de gestión de productos.");
-
-let opcion;
-
-do {
-  opcion = parseInt(prompt(
-    "Seleccione una opción:\n" +
-    "1. Agregar un producto\n" +
-    "2. Buscar un producto por nombre\n" +
-    "3. Filtrar productos por categoría\n" +
-    "4. Salir"
-  ));
-
-  switch (opcion) {
-    case 1:
-      agregarProducto();
-      break;
-    case 2:
-      buscarProducto();
-      break;
-    case 3:
-      filtrarPorCategoria();
-      break;
-    default:
-      console.log("Opción inválida. Intente nuevamente.");
-  }
-} while (opcion !== 4);
-
-console.log("¡Hasta luego!");
+// Cargar tareas desde el almacenamiento local al cargar la página
+window.addEventListener('load', cargarTareasDesdeStorage);
